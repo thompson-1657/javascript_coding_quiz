d129fa29a09f8ac2d92fc2e449a0ccc7b56a9137
Rachel Thompson <thompson.1657@gmail.com>
2020-12-02 15:05:13 -0600

    add end game page

---
+++ end.js
@@ -0,0 +1,28 @@
+var username = document.getElementById('username');
+var saveScoreBtn = document.getElementById('saveScoreBtn');
+var finalScore = document.getElementById('finalScore');
+var mostRecentScore = localStorage.getItem('mostRecentScore');
+
+var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+var MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+    e.preventDefault();
+
+    var score = {
+        score: (Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign("index.html");
+};
